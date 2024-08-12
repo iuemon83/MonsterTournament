6b159d9dcb62ef6c2f4644daffdddd4e792be6df
iuemon83 <83.program+github@gmail.com>
2024-08-12 19:53:02 +0900

    update

--- MonsterTournament/Models/BattleCard.cs
+++ MonsterTournament/Models/BattleCard.cs
@@ -7,6 +7,8 @@ namespace MonsterTournament.Client.Models
     public static class Config
     {
         public static readonly (int X, int Y) ImageSize = (250, 250);
+        public static readonly int MaxImageFileSizeInBytes = 2_000_000;
+        public static readonly int MaxCardFileSizeInBytes = 2_500_000;
     }
 
     public class AudioSrc
