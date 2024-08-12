3a20f57b2667d6698d6aa7c384eff0fc56afd5c7
iuemon83 <83.program+github@gmail.com>
2024-08-12 20:22:31 +0900

    update

--- MonsterTournament/Models/BattleCard.cs
+++ MonsterTournament/Models/BattleCard.cs
@@ -17,6 +17,8 @@ namespace MonsterTournament.Client.Models
         public static readonly string Damage = "/audio/damage.mp3";
         public static readonly string Death = "/audio/death.mp3";
         public static readonly string Action = "/audio/action.mp3";
+        public static readonly string ActionRoulette = "/audio/action_roulette.mp3";
+        public static readonly string Buff = "/audio/buff.mp3";
     }
 
     public class BattleCard
