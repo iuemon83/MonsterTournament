a59a55f28fe7ce9d24b2357907dda4eb10e77717
iuemon83 <83.program+github@gmail.com>
2024-08-12 20:42:51 +0900

    update

--- MonsterTournament/Models/BattleCard.cs
+++ MonsterTournament/Models/BattleCard.cs
@@ -13,12 +13,12 @@ namespace MonsterTournament.Client.Models
 
     public class AudioSrc
     {
-        public static readonly string Heal = "/audio/heal.mp3";
-        public static readonly string Damage = "/audio/damage.mp3";
-        public static readonly string Death = "/audio/death.mp3";
-        public static readonly string Action = "/audio/action.mp3";
-        public static readonly string ActionRoulette = "/audio/action_roulette.mp3";
-        public static readonly string Buff = "/audio/buff.mp3";
+        public static readonly string Heal = "audio/heal.mp3";
+        public static readonly string Damage = "audio/damage.mp3";
+        public static readonly string Death = "audio/death.mp3";
+        public static readonly string Action = "audio/action.mp3";
+        public static readonly string ActionRoulette = "audio/action_roulette.mp3";
+        public static readonly string Buff = "audio/buff.mp3";
     }
 
     public class BattleCard
